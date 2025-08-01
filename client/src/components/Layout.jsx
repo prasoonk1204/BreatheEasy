871a96081e3c6aff9204c29225f01174b72f55e4
NirvikD <contactnirvik25@gmail.com>
2025-08-02 00:22:27 +0530

    Fixed Layout Issues

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -11,23 +11,22 @@ const Layout = () => {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-white transition-colors duration-300">
       {/* Navbar for small screens */}
-      <Navbar 
-        isMobileMenuOpen={isMobileMenuOpen} 
-        setIsMobileMenuOpen={setIsMobileMenuOpen} 
+      <Navbar
+        isMobileMenuOpen={isMobileMenuOpen}
+        setIsMobileMenuOpen={setIsMobileMenuOpen}
       />
 
       {/* Sidebar for large screens */}
       <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
-      
+
       <div
         className={`
           flex-1 transition-all duration-300 ease-in-out
-          lg:ml-${collapsed ? "20" : "64"}
           ${isMobileMenuOpen ? "filter blur-sm" : ""}
         `}
       >
         {/* Main content area */}
-        <main className="p-4 pt-16 lg:pt-4">
+        <main className={`p-4 pt-16 lg:pt-4 ${collapsed ? "lg:ml-20" : "lg:ml-64"}`}>
           <Outlet />
         </main>
       </div>
