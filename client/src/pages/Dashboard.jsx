8de352c63c8e142c5fdbb20f8678a10a16945641
deepanshu-prajapati01 <deepanshubawaliya@gmail.com>
2025-08-01 10:25:11 +0530

    Skeleton added in dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import PollutantDetails from "../components/PollutantDetails";
 import Suggestions from "../components/Suggestions";
 import AQIScaleTable from "../components/AQIScaleTable";
 import fetchAQIData from "../utils/fetchAQIData";
+import DashboardSkeleton from "../components/skeletons/DashboardSkeleton";
 
 const Dashboard = () => {
   const [aqiData, setAqiData] = useState(null);
@@ -24,31 +25,31 @@ const Dashboard = () => {
 
   return (
     <div className="space-y-8">
-  <h2 className="text-2xl font-bold text-emerald-500 dark:text-purple-400 mt-4">
-    Your Location Air Quality
-  </h2>
+      <h2 className="text-2xl font-bold text-emerald-500 dark:text-purple-400 mt-4">
+        Your Location Air Quality
+      </h2>
 
-  {aqiData ? (
-    <>
-      <p className="text-md text-black dark:text-white font-semibold italic bg-gray-200 dark:bg-gray-700 p-6 rounded-2xl">
-        {aqiData.city}
-      </p>
+      {aqiData ? (
+        <>
+          <p className="text-md text-black dark:text-white font-semibold italic bg-gray-200 dark:bg-gray-700 p-6 rounded-2xl">
+            {aqiData.city}
+          </p>
 
-      {/* Bento grid layout for AQI and Pollutants */}
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-        <AQICard aqi={aqiData.aqi} city={aqiData.city} time={aqiData.time} />
-        <div className="lg:col-span-2">
-          <PollutantDetails components={aqiData.components} />
-        </div>
-      </div>
+          {/* Bento grid layout for AQI and Pollutants */}
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <AQICard aqi={aqiData.aqi} city={aqiData.city} time={aqiData.time} />
+            <div className="lg:col-span-2">
+              <PollutantDetails components={aqiData.components} />
+            </div>
+          </div>
 
-      <Suggestions aqi={aqiData.aqi} />
-      <AQIScaleTable />
-    </>
-  ) : (
-    <p className="text-black dark:text-white">Loading AQI data...</p>
-  )}
-</div>
+          <Suggestions aqi={aqiData.aqi} />
+          <AQIScaleTable />
+        </>
+      ) : (
+        <DashboardSkeleton />
+      )}
+    </div>
 
   );
 };
