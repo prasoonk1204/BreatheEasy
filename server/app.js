7eb99e2fffd9816b449ee01c3f7dc34f2cf65716
SujalTripathi <SujalTripathi@users.noreply.github.com>
2026-01-18 20:24:31 +0530

    feat: Register map proxy routes in Express server
    - Import and mount mapProxy routes at /api/map endpoint
    - Map tile proxy now accessible at /api/map/tiles/:style/:z/:x/:y
    - AQI overlay proxy now accessible at /api/map/aqi-overlay/:z/:x/:y

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const aqiRoutes = require('./routes/aqi');
+const mapProxyRoutes = require('./routes/mapProxy');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/aqi', aqiRoutes);
+app.use('/api/map', mapProxyRoutes);
 
 app.get('/', (req, res) => {
   res.send('BreatheEasy API is running');
